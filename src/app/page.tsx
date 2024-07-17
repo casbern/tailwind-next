e456ba8bc5c3948278c420d3699950bd260bc863
Cássia <cascrisbern@gmail.com>
2024-07-17 16:42:11 +0300

    sidebar config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-slate-900 p-8 text-slate-100">
-      <h1 className="flex items-center gap-3 text-5xl font-bold before:flex before:h-8 before:w-0.5 before:bg-sky-500">
-        Hello
-      </h1>
-      <h1 className="size-3">World</h1>
-
-      <button
-        disabled
-        className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60"
-      >
-        Signin
-      </button>
-    </div>
-  )
+  return <h1>Hello World</h1>
 }
